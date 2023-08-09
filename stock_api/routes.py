e09960486755ee8e0d902996a2906ddfcce5fa9d
Josse Carballo <jossecloud@gmail.com>
2023-08-09 13:59:17 -0400

    requires login to view homepage

--- stock_api/routes.py
+++ stock_api/routes.py
@@ -8,6 +8,7 @@ from flask_login import login_user, logout_user, login_required, current_user
 # routes to the home page if route is: / or /home
 @app.route("/")
 @app.route("/home")
+@login_required
 def home():
     return render_template('home.html')
 
