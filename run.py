db1aba4a675706061e6669294d307a5fba7a4db9
Josse Carballo <jossecloud@gmail.com>
2023-08-09 13:54:32 -0400

    adds functionality for app to create the database tables before running

--- run.py
+++ run.py
@@ -1,5 +1,11 @@
-from stock_api import app
+from stock_api import app, db
 
 # checks if the run.py file has executed directly and not imported
 if __name__ == '__main__':  
+    # create database tables before app runs
+    with app.app_context():
+        db.create_all()
+    
     app.run(debug=True)
+
+    
--- stock_api/__init__.py
+++ stock_api/__init__.py
@@ -1,16 +1,16 @@
 from flask import Flask
-from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-app.config['SECRET_KEY'] = '11adb4aa687ca51822ad4b14'
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///stock_api.db'
+app.config['SECRET_KEY'] = '11adb4aa687ca51822ad4b14' # TODO: MUST MAKE THIS AN ENV VARIABLE
+db = SQLAlchemy(app) # initializes the database using the uri provided at 'app.config['SQLALCHEMY_DATABASE_URI']'
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login" # name of the route to our login page
 login_manager.login_message_category = "info" # category for our flash message
 login_manager.login_message = "Please login to access the Market page." # flash message contents
+
 from stock_api import routes
