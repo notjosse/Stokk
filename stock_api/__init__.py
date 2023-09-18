0f7075f73abbe0e5b013285190ada8acf953d7c9
notjosse <jossec10@gmail.com>
2023-09-17 22:11:13 -0400

    restructures project to use the flask app factory method create_app()

--- stock_api/__init__.py
+++ stock_api/__init__.py
@@ -1,32 +1,27 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
-import json
 
-# Nasdaq api key
-try:
-    api_key = json.load(open('./api_key.json', 'r'))["key"]
-except:
-    api_key = "no_key"
+from .extensions import db, login_manager, bcrypt
+from .routes import main
 
+def create_app(database_uri="sqlite:///stock_api.db"):
+    # App Configs
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
+    app.config['SECRET_KEY'] = '11adb4aa687ca51822ad4b14' # TODO: MUST MAKE THIS AN ENV VARIABLE
 
-# App Configs
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///stock_api.db'
-app.config['SECRET_KEY'] = '11adb4aa687ca51822ad4b14' # TODO: MUST MAKE THIS AN ENV VARIABLE
+    # SQLAlchemy Configs
+    db.init_app(app) # initializes the database using the uri provided at 'app.config['SQLALCHEMY_DATABASE_URI']'
 
-# SQLAlchemy Configs
-db = SQLAlchemy(app) # initializes the database using the uri provided at 'app.config['SQLALCHEMY_DATABASE_URI']'
+    # Bcrypt Configs
+    bcrypt.init_app(app)
 
-# Bcrypt Configs
-bcrypt = Bcrypt(app)
+    # Login Manager Configs
+    login_manager.init_app(app)
+    login_manager.login_view = "main.login" # name of the route to our login page
+    login_manager.login_message_category = "info" # category for our flash message
+    login_manager.login_message = "Please login or register an account." # flash message contents  
 
-# Login Manager Configs
-login_manager = LoginManager(app)
-login_manager.login_view = "login" # name of the route to our login page
-login_manager.login_message_category = "info" # category for our flash message
-login_manager.login_message = "Please login or register an account." # flash message contents
+    # Routes Blueprint
+    app.register_blueprint(main)
 
-# Necessary imports
-from stock_api import routes
+    return app
