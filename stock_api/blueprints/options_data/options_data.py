41a432f11d5bf0edff221ac92d8458174e32b9b4
notjosse <jossec10@gmail.com>
2023-10-15 19:49:08 -0400

    adds /options page to the navbar, creates a temlate file and template fior the options blueprint, updates requirements.txt

--- stock_api/blueprints/options_data/options_data.py
+++ stock_api/blueprints/options_data/options_data.py
@@ -1,5 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from flask_login import login_required
+import pandas as pd
+import yfinance as yf
 
 options_data_bp = Blueprint("options_data", __name__, template_folder="templates")
 
@@ -8,4 +10,23 @@ options_data_bp = Blueprint("options_data", __name__, template_folder="templates
 @login_required
 def options():
 
-    return {"page": "options"}
+    tickers = ['AAPL', 'FB']
+    ticker_data = {}
+
+    for ticker in tickers:
+        data = yf.Ticker(ticker)
+        ticker_data[ticker] = data
+
+    expiration_dates = ticker_data['AAPL'].options
+    exp_date = expiration_dates[0]
+
+    options = ticker_data['AAPL'].option_chain(exp_date)
+
+    df = options.calls
+    options_dict = df.to_dict()
+
+    print(options_dict.keys())
+
+    return options_dict
+
+    # return render_template('options.html', data=options_dict)
